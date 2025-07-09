5035aae9d079cae08f7dc6bb449193e7f2c3ef31
MohammadReza Gh <ghavidel12th@gmail.com>
2025-07-09 22:12:26 +0330

    feat: add MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Book = require('./models/Book');
+require('dotenv').config();
 
 const app = express();
 const PORT = 4000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB!');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
 
 app.get('/', (req, res) => {
     res.send('BOOK API');
